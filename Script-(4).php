145bd584944863e71e9920ffde3bf99251300501
Monij Bhatti <142984468+MonijBhatti@users.noreply.github.com>
2023-09-02 14:56:27 +0530

    Add files via upload

---
+++ Script-(4).php
@@ -0,0 +1,14 @@
+<?php
+//Author :- Monij Bhatti
+
+/*4. Write a PHP program that assigns your first name to the variable $firstName and your last name to the variable $lastName. Use concatenation to display a greeting message like "Hello, John Doe!"*/
+
+$First_Name="Monij";
+$Last_Name="Bhatti";
+
+echo"Hello, $First_Name $Last_Name!";
+
+//Output :- 
+// Hello, Monij Bhatti!
+
+?>
---
+++ Script-(6).php
@@ -0,0 +1,16 @@
+<?php 
+//Author :- Monij Bhatti
+
+/*
+6. Develop a PHP program that assigns an initial value to the variable $x, then reassigns it to a new value, and finally echoes the value.
+*/
+
+$x=25;
+$y=$x;
+
+echo"Initial Value To The Variable : $y";
+
+//Output: -
+// Initial Value To The Variable : 25
+
+?>

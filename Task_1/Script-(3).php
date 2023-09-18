ef1d31771c2d95051b4d2eaec5babefca77dae50
unknown <monijbhatti555@gmail.com>
2023-09-18 11:31:54 +0530

    add new

---
+++ Task_1/Script-(3).php
@@ -0,0 +1,13 @@
+<?php
+//Author :- Monij Bhatti
+
+/*
+3. Develop a PHP program that assigns the value "42" to a variable named $answer and then echoes the value along with the text " is the answer to everything."
+*/
+
+$answer=42;
+echo"$answer is the answer to everything.";
+
+//Output: - 
+//42 is the answer to everything.
+?>

7de8a6589a8b18cdfaa91e082e3500b1c149987f
Monij Bhatti <142984468+MonijBhatti@users.noreply.github.com>
2023-11-30 16:27:51 +0530

    Add files via upload

--- dashbord/database/15618223036.sql
+++ dashbord/database/15618223036.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2023 at 06:46 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.8
+-- Generation Time: Nov 30, 2023 at 11:56 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,14 +32,14 @@ CREATE TABLE `about_details` (
   `about_me` varchar(555) NOT NULL,
   `skillset` varchar(555) NOT NULL,
   `project_details` varchar(555) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `about_details`
 --
 
 INSERT INTO `about_details` (`id`, `about_me`, `skillset`, `project_details`) VALUES
-(1, 'I am rahul kanjariya completed my bachelor of computer applications degree in saurashtra university. now i started Master of Computer Application in atmiya university, rajkot.I am a passionate and creative person, I like to explore as many things as possible and am curious to learn new things and apply that knowledge.', 'HTML', 'VFA - Ai Based farming Assistant\r\n\r\nVFA is a farming technology platform where we work with farmers directly. We at VFA follow our mission of bridging the gap between technology and agriculture in India with a vision to reach out to maximum Indian farmers. We support farmers to “Grow efficiently, Grow more” through the systematic implementation of scientific techniques by providing critical information at appropriate times and regular monitoring.\r\n\r\nAt VFA, we understand each farmer’s requirements, we believe every farmer deserves a chance - the cha');
+(1, 'I am Monij Bhatti completed BCA degree in Saurashtra University. now i started MCA in Atmiya University, Rajkot. I am a passionate and creative person, I like to explore as many things as possible and am curious to learn new things and apply that knowledge.', 'Backend Development', 'VFA - Ai Based farming Assistant\r\n\r\nVFA is a farming technology platform where we work with farmers directly. We at VFA follow our mission of bridging the gap between technology and agriculture in India with a vision to reach out to maximum Indian farmers. We support farmers to “Grow efficiently, Grow more” through the systematic implementation of scientific techniques by providing critical information at appropriate times and regular monitoring.\r\n\r\nAt VFA, we understand each farmer’s requirements, we believe every farmer deserves a chance - the cha');
 
 -- --------------------------------------------------------
 
@@ -68,14 +68,14 @@ CREATE TABLE `product_details` (
   `product_reviews` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `image_filenames` varchar(2555) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product_details`
 --
 
 INSERT INTO `product_details` (`product_id`, `product_name`, `product_description`, `product_category`, `product_price`, `product_sku`, `product_barcode`, `product_weight`, `product_dimensions`, `product_quantity`, `product_variants`, `shipping_info`, `seo_info`, `product_brand`, `product_features`, `product_benefites`, `related_products`, `product_reviews`, `created_at`, `image_filenames`) VALUES
-(1, 'test', 'test', 'test', '5000', 'test', 'test', '5', 'test', 10, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', '2023-11-22 05:37:10', './images/bitnami.ico');
+(1, 'test', 'test', 'test', 5000, 'test', 'test', 5, 'test', 10, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', '2023-11-22 05:37:10', './images/bitnami.ico');
 
 -- --------------------------------------------------------
 
@@ -96,15 +96,14 @@ CREATE TABLE `registration` (
   `country` varchar(15) NOT NULL,
   `education` varchar(20) NOT NULL,
   `gender` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `registration`
 --
 
 INSERT INTO `registration` (`reg_id`, `first_name`, `middle_name`, `last_name`, `email`, `mobil_no`, `password`, `city`, `state`, `country`, `education`, `gender`) VALUES
-(1, 'rahul', 'mansukhbhai', 'kanjariya', 'rahul@gmail.com', '9265614292', '123', 'jamnagar', 'gujarat', 'india', 'bca', 'male'),
-(3, 'jay', 'v', 'nakum', 'jay@gmail.com', '1234567890', '123', 'jamnagar', 'jamanagr', 'india', 'b.com', 'male');
+(1, 'Monij', 'Rustamsha', 'Bhatti', 'monijbhatti555@gmail.com', '9265215405', '1234', 'Rajkot', 'Gujarat', 'India', 'MCA', 'Male');
 
 --
 -- Indexes for dumped tables
